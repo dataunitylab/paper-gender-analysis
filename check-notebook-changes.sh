01698b6ad63889b3eada9aa38965a9dc06d1211b
Michael Mior <mmior@cs.rit.edu>
2020-11-04 14:47:32 -0500

    Exclude metadata changes when validating notebook

---
+++ check-notebook-changes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+JQ_FILTER="{cells: [.cells[] | del(.metadata)], metadata: .metadata}"
+diff <(git show HEAD:cs-paper-gender-analysis.ipynb | jq "$JQ_FILTER") <(jq "$JQ_FILTER" cs-paper-gender-analysis.ipynb)
+exit $?
