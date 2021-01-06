882e0dde3922b180216a3d4aa74d70f0daced36d
Michael Mior <mmior@cs.rit.edu>
2021-01-06 14:05:21 -0500

    Label VLDB journal as VLDBJ

--- fetch-papers.sh
+++ fetch-papers.sh
@@ -9,12 +9,13 @@ function get_dblp_json() {
     local end=$4
     local offset=$5
     local skip=$6
+    local label="${7:-$key}"
 
     # Loop over the range of parameters
     # (typically years or journal volume numbers)
     for param in $(seq $start $skip $end); do
         # Find the output filename
-        local outfile="data/$key-$(($param+$offset)).json"
+        local outfile="data/$label-$(($param+$offset)).json"
 
         # Download the file if does not exist
         url="https://dblp.org/search/publ/api?q=toc%3Adb/$type/$key/$key$param.bht%3A&h=1000&format=json"
@@ -35,7 +36,7 @@ get_dblp_json conf icde 2000 2020 0
 get_dblp_json conf sigmod 75 99 1900
 get_dblp_json conf sigmod 2000 2020 0
 
-get_dblp_json journals vldb 1 29 1991
+get_dblp_json journals vldb 1 29 1991 1 vldbj
 
 # get_dblp_json journals tods 1 45 1975
 
