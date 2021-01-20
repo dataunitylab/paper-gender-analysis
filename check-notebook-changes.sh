ddc4c9740fbaaeed54877092c197fd5ed2a7aadb
Michael Mior <mmior@cs.rit.edu>
2021-01-20 13:39:17 -0500

    Use unified diff in notebook check

--- check-notebook-changes.sh
+++ check-notebook-changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 JQ_FILTER="{cells: [.cells[] | del(.metadata)], metadata: .metadata}"
-diff <(git show HEAD:cs-paper-gender-analysis.ipynb | jq "$JQ_FILTER") <(jq "$JQ_FILTER" cs-paper-gender-analysis.ipynb)
+diff -u <(git show HEAD:cs-paper-gender-analysis.ipynb | jq "$JQ_FILTER") <(jq "$JQ_FILTER" cs-paper-gender-analysis.ipynb)
 exit $?
