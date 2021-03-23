9d097837f1ed634a860a6815bb78de2f82505aca
Michael Mior <mmior@cs.rit.edu>
2021-03-22 22:36:20 -0400

    Allow selecting field mappings

--- interactive.py
+++ interactive.py
@@ -5,11 +5,43 @@ import tabulator
 
 import analyze_genders
 
+
+FIELDS = ['field', 'paper_id', 'conf', 'year', 'authors']
+
 uploaded_file = st.file_uploader('Input file for analysis', type='csv')
+has_header = st.checkbox('CSV file has header', value=True)
 
 if uploaded_file is not None:
     file_like = io.BytesIO(uploaded_file.getvalue())
-    genders = analyze_genders.load_data_file(file_like)
+
+    # Set the header row if needed
+    if has_header:
+        header_row = 1
+    else:
+        header_row = None
+
+    # Get the column headers parsed from the file
+    stream = tabulator.Stream(file_like, scheme='stream', format='csv', headers=header_row)
+    stream.open()
+    if stream.headers:
+        file_headers = stream.headers
+    else:
+        file_headers = list(range(len(stream.sample[0])))
+
+    # Set up the field mapping
+    header_map = {}
+    for field in FIELDS:
+        # Check if there is a match already by name
+        try:
+            index = file_headers.index(field)
+        except ValueError:
+            index = len(file_headers)
+
+        value = st.selectbox(label=field, options=file_headers + [None], index=index)
+        if value is not None:
+            header_map[field] = value
+
+    genders = analyze_genders.load_data_file(file_like, header_row=header_row, header_map=header_map)
     analyze_genders.populate_genders(genders)
     df = analyze_genders.dataframe(genders)
 
